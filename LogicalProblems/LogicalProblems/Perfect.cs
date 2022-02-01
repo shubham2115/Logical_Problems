5da0eb03e383e2777a75f8b0cce7bf4cc9ee2768
Shubham <s.shrikhande1@gmail.com>
2022-02-01 22:27:48 +0530

    Perfect number

---
+++ LogicalProblems/LogicalProblems/Perfect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Perfect
+    {
+        public void perfectNumber()
+        {
+
+			Console.WriteLine("Enter a number : ");
+			int num = Convert.ToInt32(Console.ReadLine());
+			int sum = 0;
+			for (int i = 1; i < num; i++)
+			{
+				if (num % i == 0)
+					sum = sum + i;
+			}
+			if (sum == num)
+			{
+				Console.WriteLine("Perfect Number");
+			}
+			else
+			{
+				Console.WriteLine("Not an Perfect Number");
+			}
+		}
+	}
+}
+        
--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -1,6 +1,21 @@
 ﻿
 
 using LogicalProblems;
+Console.WriteLine("1.Fibonacee series\n"+
+                   "2.Perfect Number");
+int choice = Convert.ToInt32(Console.ReadLine());
 
-FIbonacee fibonacee =new FIbonacee();
-fibonacee.Series();
+switch (choice)
+{
+    case 1:
+        FIbonacee fibonacee = new FIbonacee();
+        fibonacee.Series();
+        break;
+    case 2:
+        Perfect perfect = new Perfect();
+        perfect.perfectNumber();
+        break;
+    default:
+        Console.WriteLine("Enter correct choice");
+        break;
+}
