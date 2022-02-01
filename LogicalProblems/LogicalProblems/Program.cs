ca856153e98cbb2df463ea5d2844231db0b12dd5
Shubham <s.shrikhande1@gmail.com>
2022-02-01 22:49:00 +0530

    Prime number

--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -2,7 +2,8 @@
 
 using LogicalProblems;
 Console.WriteLine("1.Fibonacee series\n"+
-                   "2.Perfect Number");
+                   "2.Perfect Number\n"+
+                   "3.Prime Number");
 int choice = Convert.ToInt32(Console.ReadLine());
 
 switch (choice)
@@ -14,6 +15,10 @@ switch (choice)
     case 2:
         Perfect perfect = new Perfect();
         perfect.perfectNumber();
+        break;
+    case 3:
+        prime prime = new prime();
+        prime.primeNumber();
         break;
     default:
         Console.WriteLine("Enter correct choice");
